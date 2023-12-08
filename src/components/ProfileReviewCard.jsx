6f61ce140c426fd950885e1db16fdfa5c6ebefcc
Mason <mason@pc-79-13.customer.ask4.lan>
2023-12-08 14:12:27 +0000

    minor css changes

--- src/components/ProfileReviewCard.jsx
+++ src/components/ProfileReviewCard.jsx
@@ -4,7 +4,7 @@ function ProfileReviewCard({ review }) {
     return (
         <>
             <Link to= {`/reviews/${review.review_id}`}>
-                <div>
+                <div className="profileReviewCard">
                     <h3>{review.title}</h3>
                     <img className= 'profileReviewImg' src={review.review_img_url}/>
                 </div>
